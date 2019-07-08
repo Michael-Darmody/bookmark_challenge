6b4473ea5b220d643b295824711665100234e0bc
Michael <darmodymichael@gmail.com>
2019-07-08 16:54:43 +0100

    Enabled data from DB to be wrapped inside an instance of Bookmark

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -2,13 +2,13 @@
 feature 'Viewing the bookmarks' do
   scenario 'a user can see all the bookmarks' do
 
-    Bookmark.create(url: "http://www.makersacademy.com")
-    Bookmark.create(url: "https://www.amazon.co.uk")
-    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create(url: "http://www.makersacademy.com", title: 'Makers Academy')
+    Bookmark.create(url: "https://www.amazon.co.uk", title: 'Amazon')
+    Bookmark.create(url: "http://www.google.com", title: 'Google')
 
     visit('/')
-    expect(page).to have_content('http://www.makersacademy.com')
-    expect(page).to have_content('http://www.google.com')
-    expect(page).to have_content('https://www.amazon.co.uk')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Amazon',  href: 'https://www.amazon.co.uk')
+    expect(page).to have_link('Google', href: 'http://www.google.com')
   end
 end
